37cd31049e271c23b5e39cf4c8914a2db05b4b0c
Reed von Redwitz <reed+github@vonredwitz.com>
2023-07-17 12:44:45 +0200

    slightly cleaner

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,32 +1,23 @@
 import { PageProps } from "$fresh/server.ts";
-import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+import { ZodObject } from "z";
 
-interface FormProps extends PageProps {
-  model: z.ZodObject<any, any, any>;
-  modelName: string;
-}
+type FormProps = PageProps<
+  { schema: ZodObject<any, any, any>; modelName: string }
+>;
 
 export default function Form(props: FormProps) {
-  const schema = props.data.schema as any as z.ZodObject<any, any, any>;
-  const test = Object.keys(schema.shape);
-  const item = props.data.item;
+  const schema = props.data.schema;
   return (
     <div>
       <form method="post" action={`/${props.data.modelName}`}>
         {Object.keys(schema.shape).map((key) => (
           <div key={key}>
             <label>{key}</label>
-            <input name={key} defaultValue={item ? item[key] : ""} />
+            <input name={key} defaultValue={""} />
           </div>
         ))}
         <button type="submit">Submit</button>
       </form>
-      {item && (
-        <div>
-          <h2>Submitted Data:</h2>
-          <pre>{JSON.stringify(item, null, 2)}</pre>
-        </div>
-      )}
     </div>
   );
 }
--- plugin/kv_admin.ts
+++ plugin/kv_admin.ts
@@ -1,80 +1,12 @@
 import { Plugin } from "$fresh/server.ts";
-import type { Handlers, PluginRoute } from "$fresh/src/server/types.ts";
+import type { HandlerContext, PluginRoute } from "$fresh/src/server/types.ts";
 import { PentagonMethods, TableDefinition } from "pentagon/mod.ts";
-import Form from "../components/Form.tsx";
-import { z } from "../tests/fixture/deps.ts";
+import { ZodObject } from "z";
 import { createMockDatabase } from "../tests/fixture/types.ts";
-import Item from "../components/Item.tsx";
-import AllItems from "../components/AllItems.tsx";
 import ModelsList from "../components/ModelsList.tsx";
-
-function generateHandlers(
-  model: PentagonMethods<TableDefinition>,
-  schema: z.ZodObject<any, any, any>,
-  modelName: string,
-): Handlers {
-  console.log(schema);
-  return {
-    GET: async (_req, ctx) => {
-      const item = await model.findFirst({
-        where: { id: ctx.params.id },
-      });
-      return ctx.render({ item, modelName });
-    },
-    POST: async (req, ctx) => {
-      //   console.log(schema);
-      //   const kv = await Deno.openKv();
-
-      //   const form = await req.formData();
-      //   const data = Object.fromEntries(form);
-      //   console.log(data);
-      //   const item2 = await model.findFirst({
-      //     where: { id: data.id },
-      //   });
-      //   if (item2) {
-      //     return ctx.render({ schema: schema, item: item2 });
-      //   }
-      //   const temp = [];
-      //   const entries = await kv.list({ prefix: ["users"] });
-      //   for await (const entry of entries) {
-      //     temp.push(entry);
-      //   }
-      //   const item = await model.create({ data });
-      //   return ctx.render({ schema: schema, item: item });
-
-      const form = await req.formData();
-      const data = Object.fromEntries(form);
-      let item = await model.findFirst({
-        where: { id: data.id },
-      });
-      if (item) {
-        return ctx.render({ schema: schema, item: item });
-      } else {
-        item = await model.create({ data });
-      }
-
-      const headers = new Headers();
-      headers.set("Location", `/${modelName}/${item.id}`);
-      return new Response(null, {
-        status: 303,
-        headers,
-      });
-    },
-    PUT: async (req, ctx) => {
-      const form = await req.formData();
-      const data = Object.fromEntries(form);
-      const item = await model.update({
-        where: { id: ctx.params.id },
-        data,
-      });
-      return ctx.render(item);
-    },
-    DELETE: async (_req, ctx) => {
-      const item = await model.delete({ where: { id: ctx.params.id } });
-      return ctx.render(item);
-    },
-  };
-}
+import AllItems from "../components/AllItems.tsx";
+import Item from "../components/Item.tsx";
+import Form from "../components/Form.tsx";
 
 export type KvPluginOptions = {
   modelPath: string;
@@ -94,17 +26,17 @@ export default async function kvPlugin(
   const realModels: string[] = [];
   for (const [modelName, tableDef] of Object.entries(db)) {
     const singularName = modelName.slice(0, -1);
-    const modStr = singularName[0].toUpperCase() + singularName.slice(1);
-    const schema = models[modStr] as unknown as z.ZodObject<any, any, any>;
+    const capitalizedModelName = singularName[0].toUpperCase() +
+      singularName.slice(1);
+    const schema = models[capitalizedModelName] as unknown as ZodObject<
+      any,
+      any,
+      any
+    >;
     if (schema === undefined) {
       continue;
     }
     realModels.push(modelName);
-    const handlers = generateHandlers(
-      tableDef as PentagonMethods<TableDefinition>,
-      schema,
-      modelName,
-    );
 
     routes.push(
       {
@@ -116,16 +48,41 @@ export default async function kvPlugin(
         path: `/${modelName}`,
         handler: {
           GET: async (_req, ctx) => {
-            const items = await tableDef.findMany({});
+            const items = await (tableDef as PentagonMethods<TableDefinition>)
+              .findMany({});
             return ctx.render({ items, modelName });
           },
-          POST: handlers.POST,
+          POST: async (req: Request, _ctx: HandlerContext) => {
+            const form = await req.formData();
+            const data = Object.fromEntries(form);
+            let item = await (tableDef as PentagonMethods<TableDefinition>)
+              .findFirst({
+                where: { id: data.id },
+              });
+            if (!item) {
+              item = await (tableDef as PentagonMethods<TableDefinition>)
+                .create({ data });
+            }
+
+            const headers = new Headers();
+            headers.set("Location", `/${modelName}/${item.id}`);
+            return new Response(null, {
+              status: 303,
+              headers,
+            });
+          },
         },
         component: AllItems,
       },
       {
         path: `/${modelName}/[id]`,
-        handler: handlers.GET,
+        handler: async (_req: Request, ctx: HandlerContext) => {
+          const item = await (tableDef as PentagonMethods<TableDefinition>)
+            .findFirst({
+              where: { id: ctx.params.id },
+            });
+          return ctx.render({ item, modelName });
+        },
         component: Item,
       },
       {
